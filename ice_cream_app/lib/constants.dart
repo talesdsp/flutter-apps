9fbb9624cbdef43c6ad1a06f5f7b9ead4621310c
tales <talesdsp@gmail.com>
2020-06-27 23:39:27 -0300

    add constants

---
+++ ice_cream_app/lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const kWhite = Color(0xFFf8f7f5);
+const kLightPink = Color(0xFFf98aab);
+const kHotPink = Color(0xFFf01358);
+const kChocolate = Color(0xFFcfb091);
+const kPistache = Color(0xFFa7d685);
+const kGrey = Color(0xFFacadad);
+const kBlack = Color(0x90000000);
+
+const kDefaultPadding = 20.0;
+
+const kDefaultShadow = BoxShadow(
+  offset: Offset(0, 4),
+  blurRadius: 4,
+  color: Colors.black26,
+);
