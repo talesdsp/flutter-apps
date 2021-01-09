bd01c2b0d4732ba4ff4b5c36322e3588bdc5aa68
tales <talesdsp@gmail.com>
2021-01-08 22:43:33 -0300

    change status bar color

--- ice_cream_app/lib/main.dart
+++ ice_cream_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ice_cream_app/constants.dart';
 import 'package:ice_cream_app/screens/Welcome/welcome_screen.dart';
@@ -10,14 +11,17 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: kPink,
+    ));
+
     return MaterialApp(
       title: 'IceCreamy',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         appBarTheme: AppBarTheme(
-          color: kLightPink,
+          color: kHotPink,
         ),
-        textSelectionColor: kLightPink,
         visualDensity: VisualDensity.adaptivePlatformDensity,
         textTheme: GoogleFonts.latoTextTheme(
           Theme.of(context).textTheme,
